0256392dcfde6728f3dec04ff6ad57cbbb3fbb73
elsvjato <svyatoslav.halenskyy@gmail.com>
2024-11-20 14:25:22 +0100

    all homeworks finished

---
+++ app/src/main/java/com/example/mobileapp/SignInActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mobileapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val registerNowLabel = findViewById<TextView>(R.id.newMember)
+
+        registerNowLabel.setOnClickListener {
+
+            Log.d("Onboarding", "Register now pressed")
+
+            val goToRegisterIntent = Intent(this@SignInActivity, CreateAccountActivity::class.java)
+            startActivity(goToRegisterIntent)
+        }
+
+
+
+
+    }
+}
